9663dd089d4ac9af462789488872b8a93af6460d
Priyanshi Gupta <152763463+priyanshiiguptaaa@users.noreply.github.com>
2024-12-05 09:57:40 +0530

    Update Login.jsx

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -29,7 +29,7 @@ const Login = () => {
 
   return (
     <div className="min-h-screen bg-[#EAEDED] flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8 bg-white p-8 rounded-lg shadow-lg">
+      <div className="max-w-md w-full space-y-8 p-8 rounded-lg shadow-lg">
         <div>
           <img
             className="mx-auto h-12 w-auto"
